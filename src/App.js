cb4a0ac68ce807afb202b0e59df585602bfb17cf
SUEHZRSSH <suehzrssh12@outlook.com>
2024-10-19 22:12:43 +0300

    contact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { toast } from "react-toastify";
+import Notification from "./Notification";
+import "./scss/App.scss";
+import { useState } from "react";
 
 function App() {
+  const [fName, setFname] = useState('');
+  const [lName, setLname] = useState('');
+  const [email, setEmail] = useState('');
+  const [msg, setMsg] = useState('');
+  const [chck, setChck] = useState();
+  const [rad, setRad] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    toast.success('Thanks for completing form, We will be in touch soon!!!')
+  }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <h2>contact us</h2>
+        <div className="names">
+          <div className="left">
+            <label htmlFor="fName">first name</label>
+            <input required value={fName} type="text" id="fName" name="fName" onChange={(e) => setFname(e.target.value)}/>
+            <div className="error">this field is required</div>
+          </div>
+          <div className="right">
+            <label htmlFor="lName">last name</label>
+            <input required value={lName} type="text" id="lName" name="lName" onChange={(e) => setLname(e.target.value)}/>
+            <div className="error">this field is required</div>
+          </div>
+        </div>
+        <div className="email">
+          <label htmlFor="email">email address</label>
+          <input required value={email} type="email" id="email" onChange={(e) => setEmail(e.target.value)}/>
+          <div className="error">this field is required</div>
+        </div>
+        <div className="types">
+          <div className="item">
+            <input required id="gen" type="radio" value='gen' name="radd" onClick={(e) => setRad('gen')}/>
+            <label htmlFor="gen">general enquiry</label>
+          </div>
+          <div className="item">
+            <input required id="ser" type="radio" value='ser' name="radd" onClick={(e) => setRad('ser')}/>
+            <label htmlFor="ser">support request</label>
+          </div>
+          <div className="error">chose one of these options</div>
+        </div>
+        <div className="msg">
+          <label htmlFor="msg">message</label>
+          <textarea required value={msg} className="mssg" id="msg" name="msg" onChange={(e) => setMsg(e.target.value)}/>
+          <div className="error">message is required</div>
+        </div>
+        <div className="sign">
+          <input required id="sign" name="sign" value='sign' type="checkbox" onChange={(e) => setChck(e.target.checked)}/>
+          <label htmlFor="sign">I consent to being contacted by the team</label>
+          <div className="error">to consent to being contacted by the team</div>
+        </div>
+        <button type="submit">submit</button>
+      </form>
+      <Notification />
     </div>
   );
 }
